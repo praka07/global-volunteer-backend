4857070241e71d99048bd597688ed6b3a4952664
praka07 <prakashbtw.007@gmail.com>
2021-03-01 12:12:34 +0000

    first commit

---
+++ src/main/java/com/global/volunteer/GlobalvolunteerApplication.java
@@ -0,0 +1,13 @@
+package com.global.volunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalvolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalvolunteerApplication.class, args);
+	}
+
+}
