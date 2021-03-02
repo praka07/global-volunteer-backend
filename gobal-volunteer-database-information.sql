145364f8f0f1913727ddbf67c2519266933c8968
praka07 <prakashbtw.007@gmail.com>
2021-03-02 04:43:49 +0000

    backend

---
+++ gobal-volunteer-database-information.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA globalvolunteer;
+
+create table roles(
+roleId int not null AUTO_INCREMENT Primary key,
+roleName varchar(20)
+)
+
+
+INSERT INTO roles(rolename) VALUES('System Administrator');
+INSERT INTO roles(rolename) VALUES('Activity Manager');
+INSERT INTO roles(rolename) VALUES('Volunteers');
+
+
+Create table users(
+userId int AUTO_INCREMENT Primary key,
+firstName varchar(15),
+lastName varchar(15) NOT NULL,
+emailId varchar(40),
+phoneNumber varchar(15),
+password varchar(15),
+active bit default(1),
+createdDate varchar(20),
+createdBy int,
+role int not null,
+FOREIGN KEY (role) REFERENCES globalvolunteer.roles(roleId)
+)
+
+
+
+INSERT INTO users(FIRSTNAME,LASTNAME,EMAILID,phoneNumber,PASSWORD,active,CreatedDate,createdBy,role)
+VALUES ('Biruk','kadu', 'birukfekadu123@gmail.com', '+8615558797197','abc123',1,CURRENT_DATE,1,1)
--- src/main/java/com/global/volunteer/GlobalvolunteerApplication.java
+++ src/main/java/com/global/volunteer/GlobalvolunteerApplication.java
@@ -3,11 +3,15 @@ package com.global.volunteer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class GlobalvolunteerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalvolunteerApplication.class, args);
+		log.info("== Application started on port == 6060 ");
 	}
 
 }
